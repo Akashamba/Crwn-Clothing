5c6d89205f74d173aec601163b8effd2e1cfd76c
Akashamba <akashambashankar@gmail.com>
2020-10-24 13:07:33 +0530

    added shop and item components

---
+++ src/components/collection/collection.component.jsx
@@ -0,0 +1,20 @@
+import { findAllByTitle } from '@testing-library/react';
+import React from 'react';
+import './collection.styles.scss';
+import Item from '../items/item.component';
+
+const Collection = ({ title, items }) => (
+    <div className='collection-preview'>
+        <h1 className='title'>{ title.toUpperCase() }</h1>
+        <div className='preview'>
+            {
+                items.filter((item, index) => (index < 4))
+                .map(({id, ...otherItemProps}) => (
+                    <Item key={id} {...otherItemProps} />
+                ))
+            }
+        </div>
+    </div>
+)
+
+export default Collection;
